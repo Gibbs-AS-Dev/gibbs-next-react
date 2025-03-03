ff532938dd2f3685b08d3796016ffb82f760e94a
sk <sk81930@gmail.com>
2025-03-03 22:20:40 +0530

    git db

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,11 +1,14 @@
-// lib/db.js
-import mysql from 'mysql2';
+import mysql from "mysql2";
 
 const connection = mysql.createPool({
-  host: 'localhost',  // e.g., localhost or a remote MySQL server
-  user: 'root',        // MySQL username (e.g., 'root')
-  password: 'Welcome@123',    // MySQL password
-  database: 'wordpress',   // WordPress database name
+  host: process.env.NEXT_DBHOST, 
+  user: process.env.NEXT_DBUSER,
+  password: process.env.NEXT_DBPASSWORD,
+  database: process.env.NEXT_DBNAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
+
 
 export default connection;
